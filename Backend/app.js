a93690550edbfa9e8e52b581d106cc1841d91400
Yam Pallace <yampalas100@gmail.com>
2023-06-04 15:46:22 +0300

    .vscode debug + new mongo

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,14 @@ const vehicles = require("./routers/vehicles");
 const users = require("./routers/users");
 const orders = require("./routers/orders");
 
-const db_url = "mongodb://127.0.0.1:27017/parker";
+const db_url = "mongodb://10.10.248.133:21771/parker";
 
-mongoose.connect(db_url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).catch(error => console.error(error));
+mongoose
+  .connect(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.error(error));
 
 const app = express();
 
