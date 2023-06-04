f2595efbf6e6931a31d46e0e780e2dea892b317b
Yam Pallace <yampalas100@gmail.com>
2023-06-04 18:37:40 +0300

    Fix the LoginScreen so that now we can press login button right after filling the fields. In addition added log out button. And one last thing, here is basketball -> 🏀

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,32 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, I18nManager } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, I18nManager } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Ionicons from '@expo/vector-icons/Ionicons';
-import MapBox from './src/components/MapBox';
-import Parking from './src/components/Parking';
-import Profile from './src/components/Profile';
-import AddParking from './src/components/AddParking';
-import MyCars from './src/components/MyCars';
-import MyParkingSpots from './src/components/MyParkingSpots';
-import RentingHistory from './src/components/RentingHistory';
-import AddCar from './src/components/AddCar';
-import MyCar from './src/components/MyCar';
-import MyParking from './src/components/MyParking';
-import LoginStackScreen from './src/screens/LoginStackScreen';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MapBox from "./src/components/MapBox";
+import Parking from "./src/components/Parking";
+import Profile from "./src/components/Profile";
+import AddParking from "./src/components/AddParking";
+import MyCars from "./src/components/MyCars";
+import MyParkingSpots from "./src/components/MyParkingSpots";
+import RentingHistory from "./src/components/RentingHistory";
+import AddCar from "./src/components/AddCar";
+import MyCar from "./src/components/MyCar";
+import MyParking from "./src/components/MyParking";
+import LoginStackScreen from "./src/screens/LoginStackScreen";
 
-import { auth } from './firebase';
-import ParkingHistoryList from './src/components/ParkingHistoryList';
-import ParkingHistoryRent from './src/components/ParkingHistoryRent';
-import RentingHistoryRent from './src/components/RentingHistoryRent';
+import { auth } from "./firebase";
+import ParkingHistoryList from "./src/components/ParkingHistoryList";
+import ParkingHistoryRent from "./src/components/ParkingHistoryRent";
+import RentingHistoryRent from "./src/components/RentingHistoryRent";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Welcome, {auth.currentUser?.displayName}!</Text>
+      <Text style={styles.header}>
+        Welcome, {auth.currentUser?.displayName}!
+      </Text>
       <MapBox navigation={navigation} />
       <StatusBar style="auto" />
     </View>
@@ -42,11 +44,11 @@ export default function App() {
   const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
-    })
+    });
     return unsubscribe;
-  }, [])
+  }, []);
 
   function HomeStackScreen() {
     return (
@@ -56,10 +58,7 @@ export default function App() {
           name="Map"
           component={HomeScreen}
         />
-        <HomeStack.Screen
-          name="Parking"
-          component={Parking}
-        />
+        <HomeStack.Screen name="Parking" component={Parking} />
       </HomeStack.Navigator>
     );
   }
@@ -72,38 +71,17 @@ export default function App() {
           name="Account"
           component={Profile}
         />
-        <ProfileStack.Screen
-          name="Edit Profile"
-          component={AddParking}
-        />
-        <ProfileStack.Screen
-          name="My Cars"
-          component={MyCars}
-        />
-        <ProfileStack.Screen
-          name="Add Car"
-          component={AddCar}
-        />
-        <ProfileStack.Screen
-          name="Add Parking"
-          component={AddParking}
-        />
+        <ProfileStack.Screen name="Edit Profile" component={AddParking} />
+        <ProfileStack.Screen name="My Cars" component={MyCars} />
+        <ProfileStack.Screen name="Add Car" component={AddCar} />
+        <ProfileStack.Screen name="Add Parking" component={AddParking} />
         <ProfileStack.Screen
           name="My Parking Spots"
           component={MyParkingSpots}
         />
-        <ProfileStack.Screen
-          name="My Parking"
-          component={MyParking}
-        />
-        <ProfileStack.Screen
-          name="Rent History"
-          component={RentingHistory}
-        />
-        <ProfileStack.Screen
-          name="Car"
-          component={MyCar}
-        />
+        <ProfileStack.Screen name="My Parking" component={MyParking} />
+        <ProfileStack.Screen name="Rent History" component={RentingHistory} />
+        <ProfileStack.Screen name="Car" component={MyCar} />
         <ProfileStack.Screen
           name="Parking History List"
           component={ParkingHistoryList}
@@ -117,46 +95,46 @@ export default function App() {
           component={RentingHistoryRent}
         />
       </ProfileStack.Navigator>
-    )
-  }
+    );
+  };
 
   return (
     <NavigationContainer style={styles.navigator}>
-      { !currentUser ?
+      {!currentUser ? (
         <LoginStackScreen />
-        :
+      ) : (
         <Tab.Navigator
           screenOptions={({ route }) => ({
             tabBarIcon: ({ focused, color, size }) => {
               let iconName;
 
-              if (route.name === 'Home') {
-                iconName = focused ? 'map' : 'map-outline';
-              } else if (route.name === 'Account') {
-                iconName = focused ? 'person' : 'person-outline';
+              if (route.name === "Home") {
+                iconName = focused ? "map" : "map-outline";
+              } else if (route.name === "Account") {
+                iconName = focused ? "person" : "person-outline";
               }
               return <Ionicons name={iconName} size={size} color={color} />;
             },
-            tabBarActiveTintColor: '#036bfc',
-            tabBarInactiveTintColor: 'gray',
+            tabBarActiveTintColor: "#036bfc",
+            tabBarInactiveTintColor: "gray",
             tabBarLabelStyle: {
-              fontWeight: 'bold',
+              fontWeight: "bold",
               fontSize: 12,
               paddingBottom: 3,
             },
             headerTitleStyle: {
-              fontWeight: 'bold',
+              fontWeight: "bold",
               fontSize: 27,
             },
             headerStyle: {
               borderBottomWidth: 0.2,
-            }
+            },
           })}
         >
           <Tab.Screen name="Home" component={HomeStackScreen} />
           <Tab.Screen name="Account" component={ProfileStackScreen} />
         </Tab.Navigator>
-      }
+      )}
     </NavigationContainer>
   );
 }
@@ -164,15 +142,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   header: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 25,
-    width: '100%',
+    width: "100%",
     marginBottom: 15,
-    marginLeft: 4
+    marginLeft: 4,
   },
 });
--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,227 +1,269 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Image, Text, TouchableOpacity, Alert, ScrollView } from 'react-native';
-import * as Location from 'expo-location'
-import { View } from 'react-native';
-import axios from 'axios';
+import React, { useEffect, useRef, useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
+import * as Location from "expo-location";
+import { View } from "react-native";
+import axios from "axios";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
 
 export default function Profile({ navigation, route }) {
-    const [useMyLocation, setUseMyLocation] = useState(false)
-    const [titleInput, setTitleInput] = useState('')
-    const [descriptionInput, setDescriptionInput] = useState('')
-    const [locationInput, setLocationInput] = useState('')
+  const [useMyLocation, setUseMyLocation] = useState(false);
+  const [titleInput, setTitleInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
+  const [locationInput, setLocationInput] = useState("");
 
-    const HomeStack = createNativeStackNavigator();
+  const HomeStack = createNativeStackNavigator();
 
-    const HomeStackScreen = () => {
-        return (
-            <HomeStack.Navigator>
-                <HomeStack.Screen
-                    options={{ headerShown: false }}
-                    name="Map"
-                    component={HomeScreen}
-                />
-                <HomeStack.Screen
-                    name="Parking"
-                    component={Parking}
-                />
-            </HomeStack.Navigator>
-        );
-    }
-
-    const onCheckBoxPress = () => {
-        setUseMyLocation(!useMyLocation)
-    }
-
-    const getMyLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-            return;
-        }
-
-        let location = await Location.getCurrentPositionAsync({
-            accuracy: Location.Accuracy.Balanced,
-            enableHighAccuracy: true,
-            timeInterval: 5
-        })
-
-        return location
-    }
-
-    const getGeoLocationFromInput = async () => {
-        const searchInputResult = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?&address=${locationInput}&key=AIzaSyCZ-6i7NFhGDzMJl1546n-2EI0laWUc2Hc`)
-        const firstSearchResult = searchInputResult.data.results[0]
-
-        let locationToReturn = null
-
-        if (firstSearchResult && firstSearchResult.geometry) {
-            locationToReturn = {
-                coords: {
-                    latitude: firstSearchResult.geometry.location.lat,
-                    longitude: firstSearchResult.geometry.location.lng,
-                }
-            }
-        }
-
-        return locationToReturn
-    }
-
-    const onCreateParking = async () => {
-        if (titleInput && descriptionInput) {
-            let currentLocation
-
-            if (useMyLocation) {
-                currentLocation = await getMyLocation()
-            } else if (locationInput) {
-                currentLocation = await getGeoLocationFromInput()
-            }
-
-            if (currentLocation && currentLocation.coords) {
-                Alert.alert('Success!', 'Parking added successfully')
-            } else {
-                Alert.alert('Failed!', `Either this parking spot doesnt exist, or youre not using own location. Please try again`)
-            }
-        } else {
-            Alert.alert('Failed!', 'Please enter all details')
-        }
-    }
-
-    const getUserDisplayName = async () => {
-        return "Moshe Jeff"
-
-        const user = await JSON.parse(await AsyncStorage.getItem('@user'));
-
-        return "Moshe Jeff"
-
-        if (user.firstName && user.lastName)
-            return (user.firstName + ' ' + user.lastName)
-        else
-            return 'moshe jeff'
-    }
-
-    const getUserDisplayName2 = () => {
-        return "Moshe Jeff"
-    }
-
+  const HomeStackScreen = () => {
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <View style={styles.headerContent}>
-                    <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar1.png' }} />
-                    <Text style={styles.name}>{getUserDisplayName2()}</Text>
-                    <View style={styles.statsContainer}>
-                        <View style={styles.statsBox}>
-                            <Text style={styles.statsLabel}>What would you like to do today?</Text>
-                        </View>
-                    </View>
-                </View>
+      <HomeStack.Navigator>
+        <HomeStack.Screen
+          options={{ headerShown: false }}
+          name="Map"
+          component={HomeScreen}
+        />
+        <HomeStack.Screen name="Parking" component={Parking} />
+      </HomeStack.Navigator>
+    );
+  };
+
+  const onCheckBoxPress = () => {
+    setUseMyLocation(!useMyLocation);
+  };
+
+  const getMyLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.Balanced,
+      enableHighAccuracy: true,
+      timeInterval: 5,
+    });
+
+    return location;
+  };
+
+  const getGeoLocationFromInput = async () => {
+    const searchInputResult = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?&address=${locationInput}&key=AIzaSyCZ-6i7NFhGDzMJl1546n-2EI0laWUc2Hc`
+    );
+    const firstSearchResult = searchInputResult.data.results[0];
+
+    let locationToReturn = null;
+
+    if (firstSearchResult && firstSearchResult.geometry) {
+      locationToReturn = {
+        coords: {
+          latitude: firstSearchResult.geometry.location.lat,
+          longitude: firstSearchResult.geometry.location.lng,
+        },
+      };
+    }
+
+    return locationToReturn;
+  };
+
+  const onCreateParking = async () => {
+    if (titleInput && descriptionInput) {
+      let currentLocation;
+
+      if (useMyLocation) {
+        currentLocation = await getMyLocation();
+      } else if (locationInput) {
+        currentLocation = await getGeoLocationFromInput();
+      }
+
+      if (currentLocation && currentLocation.coords) {
+        Alert.alert("Success!", "Parking added successfully");
+      } else {
+        Alert.alert(
+          "Failed!",
+          `Either this parking spot doesnt exist, or youre not using own location. Please try again`
+        );
+      }
+    } else {
+      Alert.alert("Failed!", "Please enter all details");
+    }
+  };
+
+  const getUserDisplayName = async () => {
+    return "Moshe Jeff";
+
+    const user = await JSON.parse(await AsyncStorage.getItem("@user"));
+
+    return "Moshe Jeff";
+
+    if (user.firstName && user.lastName)
+      return user.firstName + " " + user.lastName;
+    else return "moshe jeff";
+  };
+
+  const getUserDisplayName2 = () => {
+    return "Moshe Jeff";
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerContent}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://bootdey.com/img/Content/avatar/avatar1.png",
+            }}
+          />
+          <Text style={styles.name}>{getUserDisplayName2()}</Text>
+          <View style={styles.statsContainer}>
+            <View style={styles.statsBox}>
+              <Text style={styles.statsLabel}>
+                What would you like to do today?
+              </Text>
             </View>
-            <View style={styles.body}>
-                {/* <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('Edit Profile')}>
+          </View>
+        </View>
+      </View>
+      <View style={styles.body}>
+        {/* <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('Edit Profile')}>
                     <View style={styles.optionBody}>
                         <Text adjustsFontSizeToFit
                             style={styles.optionText}>Edit Profile</Text>
                         <Icon name="chevron-right" size={24} />
                     </View>
                 </TouchableOpacity> */}
-                <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('My Cars')}>
-                    <View style={styles.optionBody}>
-                        <Text adjustsFontSizeToFit
-                            style={styles.optionText}>My Cars</Text>
-                        <Icon name="chevron-right" size={24} />
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('My Parking Spots')}>
-                    <View style={styles.optionBody}>
-                        <Text adjustsFontSizeToFit
-                            style={styles.optionText}>My Parking Spots</Text>
-                        <Icon name="chevron-right" size={24} />
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('Rent History')}>
-                    <View style={styles.optionBody}>
-                        <Text adjustsFontSizeToFit
-                            style={styles.optionText}>Rent History</Text>
-                        <Icon name="chevron-right" size={24} />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
-};
+        <TouchableOpacity
+          style={styles.option}
+          onPress={() => navigation.navigate("My Cars")}
+        >
+          <View style={styles.optionBody}>
+            <Text adjustsFontSizeToFit style={styles.optionText}>
+              My Cars
+            </Text>
+            <Icon name="chevron-right" size={24} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.option}
+          onPress={() => navigation.navigate("My Parking Spots")}
+        >
+          <View style={styles.optionBody}>
+            <Text adjustsFontSizeToFit style={styles.optionText}>
+              My Parking Spots
+            </Text>
+            <Icon name="chevron-right" size={24} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.option}
+          onPress={() => navigation.navigate("Rent History")}
+        >
+          <View style={styles.optionBody}>
+            <Text adjustsFontSizeToFit style={styles.optionText}>
+              Rent History
+            </Text>
+            <Icon name="chevron-right" size={24} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.option} onPress={() => signOut(auth)}>
+          <View style={styles.optionBody}>
+            <Text adjustsFontSizeToFit style={styles.logOutText}>
+              LogOut
+            </Text>
+            <Icon name="chevron-right" size={24} />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'space-evenly',
-        alignContent: 'center',
-        alignItems: 'center'
-    },
-    option: {
-        width: '100%',
-        height: '20%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        borderWidth: '1px',
-        borderColor: '#a4adba',
-    },
-    optionBody: {
-        width: '80%',
-        color: '#999999',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    optionText: {
-        fontSize: 20,
-        color: '#6a717d',
-    },
-    header: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        height: '30%',
-        width: '100%'
-    },
-    headerContent: {
-        alignItems: 'center',
-    },
-    avatar: {
-        width: 130,
-        height: 130,
-        borderRadius: 63,
-        borderWidth: 4,
-        borderColor: 'white',
-        marginBottom: 10,
-    },
-    name: {
-        fontSize: 22,
-        color: '#000000',
-        fontWeight: '600',
-    },
-    statsContainer: {
-        flexDirection: 'row',
-        marginTop: 10,
-    },
-    statsBox: {
-        alignItems: 'center',
-        marginHorizontal: 10,
-    },
-    statsCount: {
-        fontSize: 18,
-        fontWeight: '600',
-        color: '#000000',
-    },
-    statsLabel: {
-        fontSize: 14,
-        color: '#999999',
-    },
-    body: {
-        alignItems: 'center',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        height: '60%',
-        width: '100%'
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  option: {
+    width: "100%",
+    height: "20%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderWidth: "1px",
+    borderColor: "#a4adba",
+  },
+  optionBody: {
+    width: "80%",
+    color: "#999999",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  optionText: {
+    fontSize: 20,
+    color: "#6a717d",
+  },
+  logOutText: {
+    fontSize: 26,
+    color: "#ff0000",
+  },
+  header: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    height: "30%",
+    width: "100%",
+  },
+  headerContent: {
+    alignItems: "center",
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 22,
+    color: "#000000",
+    fontWeight: "600",
+  },
+  statsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  statsBox: {
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  statsCount: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#000000",
+  },
+  statsLabel: {
+    fontSize: 14,
+    color: "#999999",
+  },
+  body: {
+    alignItems: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    height: "60%",
+    width: "100%",
+  },
+});
